c2761bc98df5e1c3481f0988f7ee67fe44760b2c
vaibhav-araikar <vaibhavaraikar73@gmail.com>
2026-07-02 23:18:30 +0530

    update

--- frontend/src/landing_page/home/Hero.jsx
+++ frontend/src/landing_page/home/Hero.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 function Hero() {
-  return <h1>Hero</h1>;
+  return (
+    <div className="container p-5">
+      <div className="row text-center">
+        <img src="media/homeHero.png" alt="Home Hero" className="mb-5"></img>
+        <h1 className="mt-5">Invest in everything</h1>
+        <p>
+          Online platform to invest in stocks, derivatives, mutual funds, and
+          more
+        </p>
+        <button className="p-2 signup-button btn btn-primary">
+          Signup Now
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
