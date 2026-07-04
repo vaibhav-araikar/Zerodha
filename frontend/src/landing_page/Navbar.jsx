8077a39ed8e79ed57d7fe0f9551186edc7cb7893
vaibhav-araikar <vaibhavaraikar73@gmail.com>
2026-07-04 23:34:03 +0530

    updates done

--- frontend/src/landing_page/Navbar.jsx
+++ frontend/src/landing_page/Navbar.jsx
@@ -1,7 +1,66 @@
 import React from "react";
+import "./Navbar.css";
 
 function Navbar() {
-  return <h1>Navbar</h1>;
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary border-bottom pl-5">
+      <div className="container py-3">
+        <a className="navbar-brand" href="#">
+          <img
+            src="media/logo.svg"
+            alt="Logo"
+            className="img-fluid nav-logo-image"
+          />
+        </a>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Signup
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                About
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Products
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Pricing
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Support
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
