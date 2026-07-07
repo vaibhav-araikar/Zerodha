557d46b8f997ca6a0256fdd852a7b8d0ed89a89a
vaibhav-araikar <vaibhavaraikar73@gmail.com>
2026-07-07 23:59:53 +0530

    added

--- frontend/src/landing_page/Footer.jsx
+++ frontend/src/landing_page/Footer.jsx
@@ -1,7 +1,92 @@
 import React from "react";
 
 function Footer() {
-  return <h1>Footer</h1>;
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <img
+            src="media/logo.svg"
+            alt="Zerodha Logo"
+            style={{ width: "50%" }}
+          />
+        </div>
+        <div className="col">
+          <p>Account</p>
+          <a href="">Open demat account</a>
+          <br></br>
+          <a href="">Minor demat account</a>
+          <br></br>
+          <a href="">NRI demat account</a>
+          <br></br>
+          <a href="">HUF demat account</a>
+          <br></br>
+          <a href="">Commodity</a>
+          <br></br>
+          <a href="">Dematerialisation</a>
+          <br></br>
+          <a href="">Fund transfer</a>
+          <br></br>
+          <a href="">MTF</a>
+          <br></br>
+        </div>
+        <div className="col">
+          <p>Support</p>
+          <a href="">Contact us</a>
+          <br></br>
+          <a href="">Support portal</a>
+          <br></br>
+          <a href="">How to file a complaint?</a>
+          <br></br>
+          <a href="">Status of your complaints</a>
+          <br></br>
+          <a href="">Bulletin</a>
+          <br></br>
+          <a href="">Circular</a>
+          <br></br>
+          <a href="">Z-Connect blog</a>
+          <br></br>
+          <a href="">Downloads</a>
+        </div>
+        <div className="col">
+          <p>Company</p>
+          <a href="">About</a>
+          <br></br>
+          <a href="">Philosophy</a>
+          <br></br>
+          <a href="">Press & media</a>
+          <br></br>
+          <a href="">Careers</a>
+          <br></br>
+          <a href="">Zerodha Cares (CSR)</a>
+          <br></br>
+          <a href="">Zerodha.tech</a>
+          <br></br>
+          <a href="">Open source</a>
+          <br></br>
+          <a href="">Referral program</a>
+        </div>
+        <div className="col">
+          <p>Quick links</p>
+          <a href="">Upcoming IPOs</a>
+          <br></br>
+          <a href="">Brokerage charges</a>
+          <br></br>
+          <a href="">Market holidays</a>
+          <br></br>
+          <a href="">Economic calendar</a>
+          <br></br>
+          <a href="">Calculators</a>
+          <br></br>
+          <a href="">Markets</a>
+          <br></br>
+          <a href="">Sectors</a>
+          <br></br>
+          <a href="">Gift Nifty</a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
