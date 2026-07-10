6fa2dbb946c8041339b92494d763113b4be075ed
vaibhav-araikar <vaibhavaraikar73@gmail.com>
2026-07-10 21:35:18 +0530

    routes created

---
+++ frontend/src/landing_page/about/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div className="container">
+      <div className="row">
+        <h2>
+          We pioneered the discount broking model in India. Now, we are breaking
+          ground with our technology.
+        </h2>
+      </div>
+    </div>
+  );
+}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,36 +1,38 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import "./index.css";
-// import App from "./App.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Homepage from "./landing_page/home/HomePage.jsx";
-import Signup from "./landing_page/signup/Signup.jsx";
-import Awards from "./landing_page/home/Awards.jsx";
-import Education from "./landing_page/home/Education.jsx";
-import Hero from "./landing_page/home/Hero.jsx";
+
+// Common Components
 import Navbar from "./landing_page/Navbar.jsx";
 import Footer from "./landing_page/Footer.jsx";
-import Pricing from "./landing_page/home/Pricing.jsx";
-import Stats from "./landing_page/home/Stats.jsx";
+
+// Pages
+import Homepage from "./landing_page/home/HomePage.jsx";
+import Signup from "./landing_page/signup/Signup.jsx";
+import AboutPage from "./landing_page/about/AboutPage.jsx";
+import ProductPage from "./landing_page/products/Hero.jsx";
+import PricingPage from "./landing_page/pricing/PricingPage.jsx";
+import SupportPage from "./landing_page/support/SupportPage.jsx";
 import NotFound from "./landing_page/NotFound.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <StrictMode>
+  <StrictMode>
+    <BrowserRouter>
       <Navbar />
+
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/awards" element={<Awards />} />
-        <Route path="/education" element={<Education />} />
-        <Route path="/hero" element={<Hero />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/stats" element={<Stats />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/products" element={<ProductPage />} />
+        <Route path="/pricing" element={<PricingPage />} />
+        <Route path="/support" element={<SupportPage />} />
+
+        {/* 404 Page */}
         <Route path="*" element={<NotFound />} />
-        // * means agar kisi bhi uppar wale routes ke liye match nahi hua to ye
-        route chalega
       </Routes>
+
       <Footer />
-    </StrictMode>
-  </BrowserRouter>,
+    </BrowserRouter>
+  </StrictMode>,
 );
