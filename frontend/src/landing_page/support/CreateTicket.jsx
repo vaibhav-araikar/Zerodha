b82146abb69b5eb18f5b94b9d34006ddd829b1ce
vaibhav-araikar <vaibhavaraikar73@gmail.com>
2026-07-11 23:35:55 +0530

    new components are created

---
+++ frontend/src/landing_page/support/CreateTicket.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CreateTicket() {
+  return <h1>Create Ticket</h1>;
+}
---
+++ frontend/src/landing_page/support/Hero.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function HeroSupport() {
+  return <h1>Support page of Hero</h1>;
+}
+
+export default HeroSupport;
