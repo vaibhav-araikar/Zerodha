930faecfcd1f5a9cb6a9de83106e8471510f3ee3
vaibhav-araikar <vaibhavaraikar73@gmail.com>
2026-07-16 23:09:02 +0530

    changes done

--- frontend/src/landing_page/about/AboutPage.jsx
+++ frontend/src/landing_page/about/AboutPage.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Hero from "./Hero";
+import Team from "./Team";
 
 function AboutPage() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <Team />
+    </>
+  );
 }
 
 export default AboutPage;
