78c6a6489f45da50d009d24012b89fd431c937a9
vaibhav-araikar <vaibhavaraikar73@gmail.com>
2026-07-22 21:26:52 +0530

    pricing section done

--- frontend/src/landing_page/pricing/Brokerage.jsx
+++ frontend/src/landing_page/pricing/Brokerage.jsx
@@ -498,6 +498,23 @@ function Brokerage() {
           </div>
         </div>
       </section>
+      <h4 className="mt-4 mb-3" style={{ fontSize: "1rem" }}>
+        Disclaimer
+      </h4>
+      <p className="mb-5 pb-5" style={{ fontSize: ".75rem" }}>
+        For Delivery based trades, a minimum of ₹0.01 will be charged per
+        contract note. Clients who opt to receive physical contract notes will
+        be charged ₹20 per contract note plus courier charges. Brokerage will
+        not exceed the rates specified by SEBI and the exchanges. All statutory
+        and regulatory charges will be levied at actuals. Brokerage is also
+        charged on expired, exercised, and assigned options contracts. Free
+        investments are available only for our retail individual clients.
+        Companies, Partnerships, Trusts, and HUFs need to pay 0.1% or ₹20
+        (whichever is less) as delivery brokerage. A brokerage of 0.25% of the
+        contract value will be charged for contracts where physical delivery
+        happens. For netted off positions in physically settled contracts, a
+        brokerage of 0.1% will be charged.
+      </p>
     </div>
   );
 }
