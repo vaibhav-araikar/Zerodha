1622a30b15c394d6b87f6992aef5cfb2beaff60d
vaibhav-araikar <vaibhavaraikar73@gmail.com>
2026-07-25 22:52:14 +0530

    ticket creattion section pending

--- frontend/src/landing_page/support/CreateTicket.jsx
+++ frontend/src/landing_page/support/CreateTicket.jsx
@@ -1,5 +1,92 @@
 import React from "react";
 
 export default function CreateTicket() {
-  return <h1>Create Ticket</h1>;
+  return (
+    <div className="container">
+      <div className="row">
+        {/* left section */}
+        <div className="col-8"></div>
+        <div class="input-group flex-nowrap mt-5 pt-1 border">
+          <span class="input-group-text border" id="addon-wrapping">
+            <i class="fa-solid fa-circle-plus"></i>
+          </span>
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Username"
+            aria-label="Username"
+            aria-describedby="addon-wrapping"
+          ></input>
+        </div>
+
+        <div class="input-group flex-nowrap mt-3">
+          <span class="input-group-text" id="addon-wrapping">
+            <i class="fa-regular fa-circle-user"></i>
+          </span>
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Username"
+            aria-label="Username"
+            aria-describedby="addon-wrapping"
+          ></input>
+        </div>
+
+        <div class="input-group flex-nowrap mt-3">
+          <span class="input-group-text" id="addon-wrapping">
+            <i class="fa-brands fa-xing"></i>
+          </span>
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Username"
+            aria-label="Username"
+            aria-describedby="addon-wrapping"
+          ></input>
+        </div>
+
+        <div class="input-group flex-nowrap mt-3">
+          <span class="input-group-text" id="addon-wrapping">
+            <i class="fa-solid fa-indian-rupee-sign"></i>
+          </span>
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Username"
+            aria-label="Username"
+            aria-describedby="addon-wrapping"
+          ></input>
+        </div>
+
+        <div class="input-group flex-nowrap mt-3">
+          <span class="input-group-text" id="addon-wrapping">
+            <i class="fa-solid fa-palette"></i>
+          </span>
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Username"
+            aria-label="Username"
+            aria-describedby="addon-wrapping"
+          ></input>
+        </div>
+
+        <div class="input-group flex-nowrap mt-3">
+          <span class="input-group-text" id="addon-wrapping">
+            <i class="fa-solid fa-coins"></i>
+          </span>
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Username"
+            aria-label="Username"
+            aria-describedby="addon-wrapping"
+          ></input>
+        </div>
+
+        {/* right section */}
+        <div className="col-4"></div>
+      </div>
+    </div>
+  );
 }
