36ae312bbecde80f9dc85e20484a7e4980832d29
vaibhav-araikar <vaibhavaraikar73@gmail.com>
2026-07-29 21:21:17 +0530

    changes are ongoing

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -1,12 +1,16 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import "./index.css";
+import Home from "./components/Home";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
 );
