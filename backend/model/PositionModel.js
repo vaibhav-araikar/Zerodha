b962134d10011660e22ab56957ced2aa97f9610a
vaibhav-araikar <vaibhavaraikar73@gmail.com>
2026-08-06 23:21:14 +0530

    changes done

--- backend/model/PositionModel.js
+++ backend/model/PositionModel.js
@@ -4,3 +4,6 @@ const Position = require("../schemas/HoldingSchema");
 const PositionModel = model("Position", PositionSchema);
 
 module.exports = PositionModel;
+
+// Vaibhav Araikar
+// G H Raisoni College of Engineering, Nagpur
