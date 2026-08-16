0154c77146444573442cbe79e06309a80af9f418
vaibhav-araikar <vaibhavaraikar73@gmail.com>
2026-08-16 22:37:28 +0530

    done

--- dashboard/src/components/GeneralContext.jsx
+++ dashboard/src/components/GeneralContext.jsx
@@ -1,7 +1,9 @@
+// this file is the handler for the general context of the application. It provides a context for opening and closing the buy action window, which is used to buy stocks. The context is provided to all components that need to access it, allowing them to open or close the buy action window as needed.
 import React, { useState } from "react";
 
 import BuyActionWindow from "./BuyActionWindow";
 
+// context is generally piece of item jo multiple components share karenge
 const GeneralContext = React.createContext({
   openBuyWindow: (uid) => {},
   closeBuyWindow: () => {},
