42e7e243b895e378f1e371136779e5f7987b9918
vaibhav-araikar <vaibhavaraikar73@gmail.com>
2026-08-19 22:54:17 +0530

    testing scripts added

---
+++ frontend/test/Hero.test.js
@@ -0,0 +1,16 @@
+// jo component ko test karna hai uska naam first me then test then js
+// Hero.test.js
+
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Hero from "../src/landing_page/home/Hero.jsx";
+
+describe("Hero Component", () => {
+  test("renders the Hero component with correct content", () => {
+    render(<Hero />);
+    const heroImage = screen.getByAltText("Home Hero");
+    expect(heroImage).toBeInTheDocument();
+    expect(heroImage).toHaveAttribute("src", "media/homeHero.png");
+  });
+});
