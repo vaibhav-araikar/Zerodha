856e2e814a05e8f6c1f4e91bd7f269e3e0cb787a
vaibhav-araikar <vaibhavaraikar73@gmail.com>
2026-08-23 00:29:39 +0530

    errors are not solved yet

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Home from "./components/Home";
-import AuthPage from "./users/AuthPage";
+import AuthPage from "./Users/AuthPage";
 import { AuthProvider } from "./Users/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
